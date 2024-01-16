10bb6cdbf6dc4fc36d56a3335eeb7b4c108e8b4a
jatwing <jatwing@proton.me>
2024-01-16 19:27:26 +0800

    fix how we link rc.lua.

--- awesome/link.sh
+++ awesome/link.sh
@@ -1,2 +1,5 @@
 mkdir -p ~/.config/awesome
 ln -sf $(dirname $(realpath $0))/rc.lua ~/.config/awesome/rc.lua
+
+[ -L ~/.config/awesome/modules ] && unlink ~/.config/awesome/modules
+ln -s $(dirname $(realpath $0))/modules ~/.config/awesome/modules
