1a0be4191ec5d6ed921847278194c86bfd49d6b2
user <user@localhost.localdomain>
2024-01-25 22:30:44 +0800

    backup awesome

--- awesome/link.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ~/.config/awesome
-ln -sf $(dirname $(realpath $0))/rc.lua ~/.config/awesome/rc.lua
-
-[ -L ~/.config/awesome/modules ] && unlink ~/.config/awesome/modules
-ln -s $(dirname $(realpath $0))/modules ~/.config/awesome/modules
